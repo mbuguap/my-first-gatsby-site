ffb89c81ac0a7b8ca9ffc0e97eb3fca3ee9a9f27
Peter Mbugua <petrmbugua@yahoo.com>
2022-04-05 17:57:11 +0300

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,20 @@
 import * as React from 'react';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p>I'm making this by following the Gatsby tutorial</p>
+      <StaticImage
+        alt='Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera'
+        src='https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large'
+      />
+      {/* Local Image */}
+      {/* <StaticImage
+        alt='Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera'
+        src='../images/dog.webp'
+      /> */}
     </Layout>
   );
 };
